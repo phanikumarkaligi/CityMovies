354dbfc265e72d3cfd75a2cc8f6462b4f14fb5e7
phanikumarkaligi <phanikumar.kaligi@gmail.com>
2016-03-14 12:30:28 +0530

    Initial Commit

---
+++ CityMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CityMovies
+//
+//  Created by Deepthi Kaligi on 14/03/2016.
+//  Copyright © 2016 MightyDevelopers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
